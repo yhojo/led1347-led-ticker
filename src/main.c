b72a72692019c814da0627b632bd25c1569a1341
Yasuo Hojo <yhojo@pp.iij4u.or.jp>
2014-02-22 15:42:23 +0900

    forループでタイミング調整するLチカの完成

--- src/main.c
+++ src/main.c
@@ -18,15 +18,39 @@
 
 // TODO: insert other definitions and declarations here
 
+// LEDのポート番号と、ピン番号を定義
+#define LED_PORT 0
+#define LED_PIN 1
+
 int main(void) {
 
     // TODO: insert code here
+	// GPIOドメインの電源を入れる
+	LPC_SYSCON->SYSAHBCLKCTRL |= (1<<6);
+	// LED用のGPIOピンを出力に設定
+	LPC_GPIO->DIR[LED_PORT] |= (1 << LED_PIN);
 
+	// LEDの次の出力状態を保持する変数
+	int led_on = 0;
     // Force the counter to be placed into memory
     volatile static int i = 0 ;
     // Enter an infinite loop, just incrementing a counter
     while(1) {
         i++ ;
+        // led_onに合わせてLEDを点灯したり消灯したりする
+        if (led_on) {
+        	// 点灯する
+        	LPC_GPIO->SET[LED_PORT] = (1 << LED_PIN);
+        } else {
+        	// 消灯する
+        	LPC_GPIO->CLR[LED_PORT] = (1 << LED_PIN);
+        }
+        // led_onの状態を反転する
+        led_on = !led_on;
+
+        // ちょっと待つ
+        for (long j = 0; j < 1000000; j++) {
+        }
     }
     return 0 ;
 }
