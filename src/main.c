b6deb424f1b803bff3cadd291a753fd6fa7703de
Yasuo Hojo <yhojo@pp.iij4u.or.jp>
2014-02-22 15:58:58 +0900

    GPIO操作を関数化してコードを整理した。

--- src/main.c
+++ src/main.c
@@ -18,24 +18,45 @@
 #define LED_PORT 0
 #define LED_PIN 1
 
-int main(void) {
+void gpio_init() {
 	// GPIOドメインの電源を入れる
 	LPC_SYSCON->SYSAHBCLKCTRL |= (1<<6);
+}
+
+/**
+ * GPIOのピンの入出力方向を設定する。
+ */
+void gpio_set_dir(int port, int pin, int do_output) {
+	if (do_output) {
+		LPC_GPIO->DIR[port] |= (1 << pin);
+	} else {
+		LPC_GPIO->DIR[port] &= ~(1 << pin);
+	}
+}
+
+/**
+ * GPIOの特定ピンの出力内容を設定する。
+ */
+void gpio_set_output(int port, int pin, int enable) {
+    if (enable) {
+    	LPC_GPIO->SET[port] = (1 << pin);
+    } else {
+    	LPC_GPIO->CLR[port] = (1 << pin);
+    }
+}
+
+int main(void) {
+	// GPIOの初期化
+	gpio_init();
 	// LED用のGPIOピンを出力に設定
-	LPC_GPIO->DIR[LED_PORT] |= (1 << LED_PIN);
+	gpio_set_dir(LED_PORT, LED_PIN, 1);
 
 	// LEDの次の出力状態を保持する変数
 	int led_on = 0;
-    // Enter an infinite loop, just incrementing a counter
+    // Enter an infinite loop
     while(1) {
         // led_onに合わせてLEDを点灯したり消灯したりする
-        if (led_on) {
-        	// 点灯する
-        	LPC_GPIO->SET[LED_PORT] = (1 << LED_PIN);
-        } else {
-        	// 消灯する
-        	LPC_GPIO->CLR[LED_PORT] = (1 << LED_PIN);
-        }
+    	gpio_set_output(LED_PORT, LED_PIN, led_on);
         // led_onの状態を反転する
         led_on = !led_on;
 
